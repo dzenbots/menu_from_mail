516455925cfa9d09ff4b4e0cde383567946c33f8
dzenbots <vv.khritkin@gmail.com>
2021-03-09 18:24:46 +0300

    some fixes

--- site_worker/site_worker.py
+++ site_worker/site_worker.py
@@ -1,8 +1,6 @@
-import json
 import os
 import random
 import string
-import sys
 
 import requests
 from bs4 import BeautifulSoup
@@ -50,52 +48,34 @@ class SiteWorker(requests.Session):
             print('Authorization error')
             return False
 
-    def get_file_info(self, id):
-        print(f'Searching file id: {id}')
-        self.headers.update({'Accept': 'application/json, text/javascript, */*; q=0.01'})
-        response = self.get(self.base + '/elfinder/files')
-        if response.status_code != 200:
-            print('Something is wrong!')
-            return None
-        while True:
-            request_str = self.base + '/efconnect/files?mode='
-            request_str += f'&cmd=open&target={id}'
-            response = self.get(request_str)
-            if response.status_code == 200:
-                break
-            else:
-                print('Trying again...')
-        return response.json()
-
     def search_folder_id(self, root_folder_id, folder_path):
         cur_path_id = root_folder_id
         path = folder_path.split('/')
         search_folder_name = 'files'
         for i in range(1, len(path)):
             search_folder_name = path[i]
-            while True:
-                files = self.get_file_info(cur_path_id)
-                if files is not None:
-                    break
-            # while True:
-            #     info = self.get_file_info(cur_path_id)
-            #     if info:
-            #         files = info.get('files')
-            #         if files:
-            #             break
-            # if not files:
-            print(json.dumps(files, indent=4, sort_keys=True))
-            files = {item.get('name'): item.get('hash') for item in files.get('files')}
+            files = {item.get('name'): item.get('hash') for item in self.get_file_info(cur_path_id).get('files')}
             if search_folder_name not in files.keys():
                 print(f'Path {folder_path} does not exist!')
                 return None
             cur_path_id = files.get(search_folder_name)
         return {search_folder_name: files.get(search_folder_name)}
 
+    def get_file_info(self, id):
+        self.headers.update({'Accept': 'application/json, text/javascript, */*; q=0.01'})
+        response = self.get(self.base + '/elfinder/files')
+        if response.status_code != 200:
+            print('Something is wrong!')
+            return
+        request_str = self.base + '/efconnect/files?mode='
+        request_str += f'&cmd=open&target={id}'
+        response = self.get(request_str)
+        return response.json()
+
     def get_url(self, url):
         return BeautifulSoup(self.get(self.base + url).text, 'html.parser').prettify()
 
-    def proccess_upload(self, folder_path, filename, binary_file):
+    def proccess_upload(self, folder_path, filename, binary_file, fsize):
         request_str = self.base + '/efconnect/files'
         request_str += f'?mode=&cmd=upload&target={folder_path}&name[]:{filename}'
         response = self.get(request_str)
@@ -104,23 +84,21 @@ class SiteWorker(requests.Session):
                 'cmd': 'upload',
                 'target': folder_path,
                 'suffix': '~',
-                # 'upload[]': str(binary_file).encode('utf-8')
-            }
-            files = {
-                'upload[]': binary_file
+                'upload[]': (f'{filename}', binary_file, 'application/pdf'),
+                'uploading_path[]': ''
             }
             m = MultipartEncoder(fields=data, boundary='----WebKitFormBoundary' + ''.join(
                 random.sample(string.ascii_letters + string.digits, 16)))
-            # m = MultipartEncoder(fields=data)
-
+            # print(m.parts)
             self.headers.update(
                 {'Content-Type': m.content_type})
             # print(m.content_type)
             # self.headers.update(
-            #     {'Content-Length': str(len(binary_file))})
-            # print(len(str(binary_file).encode('utf-8')))
+            #     {'Content-Length': f'{fsize}'})
+
             response = self.post(self.base + '/efconnect/files?mode=',
-                                 json=data, files=files)
+                                 data=m)
+
             if response.status_code == 200:
                 print(response.json())
                 print('File is uploaded')
@@ -133,11 +111,12 @@ class SiteWorker(requests.Session):
                                             folder_path=folder_path)
         if folder_info:
             filename = os.path.basename(file_path)
+            print(filename)
+            fsize = os.stat(file_path).st_size
+            print(fsize)
             with open(file_path, 'rb') as fb:
                 binary_file = fb.read()
-                # print(len(binary_file))
+                # print(binary_file)
                 key, value = folder_info.popitem()
-                self.proccess_upload(folder_path=value, filename=filename, binary_file=fb)
+                self.proccess_upload(folder_path=value, filename=filename, binary_file=fb, fsize=fsize)
                 fb.close()
-        else:
-            return
